c1b2dcd3c24a371a6e191ded0e6d96e169260b78
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-14 11:08:53 -0400

    add read me an dir of repo

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - Entry piont
+*Return: Always 0 (Sucssesfuly)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
