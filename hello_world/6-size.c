98c1da18bd17352e4ba359827d0460fc2bf77790
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-14 14:42:33 -0400

    ad task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: Always 0 (succesfuly)
+*/
+int main(void)
+{
+printf(sizeof "of a"  char);
+printf(sizeof "of an" int);
+printf(sizeof "of a long int" long int);
+printf(sizeof "of a long long int" long long int);
+printf(sizeof "of a" float);
+
+return (0);
+}
