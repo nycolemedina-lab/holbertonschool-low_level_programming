e2d74e76860f461e0d1cd216eb0f4387ebb098f0
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-16 13:26:55 -0400

    add task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+{
+	printf("%d\n", i);
+}
+	return (0);
+}
