82d0817fb5da7a7afd1de5b790eb48d429c5790f
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-18 08:15:40 -0400

    add task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
 * main - Entry point
 * Return: Always 0 (Success)
@@ -6,6 +6,15 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar("_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+
 	return (0);
 }
