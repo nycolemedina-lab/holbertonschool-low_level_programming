a2baa4b3bd79a1207eba1b85c7971ffd0adb65c8
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-18 20:43:40 -0400

    add task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_last_digit - Prints the last digit of a number
+* @l: The number to extract from
+*
+*Return: The value for the last digit
+*/
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+{
+	l = -l;
+}
+	_putchar(l + '0');
+	return (l);
+}
