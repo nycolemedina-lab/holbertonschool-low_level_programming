a68b096db7bf32a651e9d9228f88d2d33ef6d852
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-20 11:28:22 -0400

    add task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - Prints the numbers from zero to nine followed by a new line
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+{
+	_putchar(i + '0');
+	i++
+}
+	_putchar('\n');
+}
