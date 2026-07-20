d9625747226c3a790ccf33559b819935600cf0bd
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-20 13:28:14 -0400

    add task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* more_numbers - Prints the numbers zero to fourteen ten times
+* followed by a new line
+*
+* Return: void
+*/
+void more_numbers(void)
+{
+	int i, e;
+
+	for (i = 0 ; i <= 10 ; i++)
+{
+	for (e = 0 ; e <= 14 ; e++)
+{
+	_putchar(i = '0');
+}
+	_putchar('\n');
+}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -13,5 +13,7 @@ void print_numbers(void);
 
 void print_most_numbers(void);
 
+void more_numbers(void);
+
 
 #endif
