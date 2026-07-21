fbcf3f8334df5d7aaa23c1062f2c7710165e8b19
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-21 13:17:57 -0400

    add task 9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* print_triangle - prints a triangle follower by a new line
+* @size: size of the triangle
+*
+* Return: void
+*/
+void print_triangle(int size)
+{
+	int a, e, i;
+
+	if (size > 0)
+	{
+	for (a = 0 ; a < size ; a++)
+	{
+		for (e = 0 ; e < size -a -1 ; e++)
+		{
+			_putchar(' ');
+		}
+		for (i = 0 ; i <=a ; i++)
+		{
+			_putchar('#');
+		{
+		_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
