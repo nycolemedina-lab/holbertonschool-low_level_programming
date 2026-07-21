916fb5d2ae7d1d1f97439bbcf55a6643ef5d4537
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-21 10:40:15 -0400

    add task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_aquare - prints a square followed by a new line
+* @size: size if the square
+*
+* Return: void
+*/
+void print_square(int size)
+{
+	int a, e;
+
+	if (size > 0)
+{
+	for (a = 0 ; a < size ; a++)
+{
+	for (e = 0 ; e < size ; e++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
+	else
+{
+	_putchar('\n');
+}
+}
