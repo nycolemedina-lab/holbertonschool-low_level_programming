c9682d0d76f4f61683312835ed12ff39876371a1
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-23 14:06:19 -0400

    add task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* swap_int - swaps the value of two integers
+* @a: int to check
+* @b: int to chec
+*
+* Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	*a = b;
+
+	*b = a;
+
+}
