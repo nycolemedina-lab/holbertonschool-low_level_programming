aa8a50e965eea7e21307c5c851a4f24a431133b9
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-24 12:42:09 -0400

    add task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* rev_string - Reverses a string
+* @s: pointer whith the string
+*
+* Returns: void
+*/
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = b - 1;
+
+	while (s[a] != '\0')
+	{
+		while (b >= 0)
+		{
+			a = b;
+			a++;
+		{
+		b++;
+	}
+
+}
