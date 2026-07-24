86176fa0417a2f8c3440f87106522eade09a4d52
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-24 14:45:14 -0400

    add task 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,33 +7,29 @@
 */
 void puts_half(char *str)
 {
-	int len;
+	int len = 0;
 	int a;
 	int n;
 
-	while (*str != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
-		str++;
 	}
-	int half = len / 2;
 
-	if (len % 2 = 0)
+	if (len % 2 == 0)
 	{
-		for (a = half ; a != '\0' ; a++)
-		{
-			_putchar(str[a]);
-		}
+		n = len / 2;
 	}
 	else
 	{
 		n = (len + 1) / 2;
-
-		for (a = n ; a != '\0' ; a++)
-		{
-			_putchar(str[a]);
-		}
 	}
+
+	for (a = n ; a < len ; a++)
+                {
+                        _putchar(str[a]);
+                }
+
 	_putchar('\n');
 
 }
