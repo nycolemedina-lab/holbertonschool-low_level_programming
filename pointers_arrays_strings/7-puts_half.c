ac2fa4b693a8fa3e6ea34662bb18b2cbba209795
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-24 14:26:55 -0400

    add task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* put_half - prtints the second half of a string, followed by a new line
+* @str: pointer with the string
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	int half = len / 2;
+
+	if (len % 2 = 0)
+	{
+		for (a = half ; a != '\0' ; a++)
+		{
+			_putchar(str[a]);
+		}
+		else
+		{
+			n = (len + 1) / 2
+			for (a = n ; a != '\0' ; a++)
+			{
+				_putchar(str[a]);
+			}
+		}
+	}
+	_putchar('\n');
+
+}
