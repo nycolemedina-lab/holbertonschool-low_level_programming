efabc07c1a29b6624e53412515f88fd7d8298c9e
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-25 00:21:11 -0400

    add task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _scrcpy - copies the string in src whit the null, to dest
+* @dest: pointer to the destination buffer
+* @src: pointer to the source string
+*
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+
+}
