e0aae5dbb42834e17f0f9dc1c9f91aeaed30479b
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-26 17:22:35 -0400

    add task 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
+* reverse_array - reverse an array of integers
+* @a: pointer to the array of integers
+* @n: number of elements in the array
 *
-*
-*
-*
-*
-*
+* Return: void
 */
 void reverse_array(int *a, int n)
 {
