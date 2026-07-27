f9e55153f639071ef98d259f59650f03d1ded13f
nycolemedina-lab <nycole.medina@gmail.com>
2026-07-26 20:50:55 -0400

    add task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* cap_string - capitalizes all words of a string
+* @s; thring to modify
+*
+* Return: pointer to the modified string
+*/
+char *cap_string(char *s)
+{
+	int len = 0;
+	int a;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (s[len] != '\0')
+	{
+
+		if (s[len] >= 'a' && s[len] <= 'z')
+		{
+			if (len == 0)
+			{
+				s[len] -= 32;
+			}
+			else
+			{
+				for (a = 0 ; sep[a] != '\0' ; a++)
+				{
+					if (s[len - 1] == sep[a])
+					{
+						s[len] -= 32;
+						break;
+					}
+				}
+			}
+		}
+		len++;
+	}
+	return (s);
+}
