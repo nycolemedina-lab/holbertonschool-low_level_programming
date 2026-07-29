d458802c648c148edc459244b776cdce2fdf7bfe
nycolemedina-lab <nycole.medina@gmain.com>
2026-07-29 11:25:55 -0400

    add task 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+* memset - fills the first n byte of the memory area pointed by with the costant byte b
+* @s: memory area to filled
+* @b: byte to fill with
+* @n: number of byte to fill
+*
+* Return: pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0 ; a < n ; a++)
+	{
+		s[a] = b
+	}
+	return (0);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -41,5 +41,9 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+char *_memset(char *s, char b, unsigned int n);
+
+
+
 #endif
 
