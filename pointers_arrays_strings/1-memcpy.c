7c3d8347e7e4d6af9b73cb1723d19fdc719a833d
nycolemedina-lab <nycole.medina@gmain.com>
2026-07-29 12:48:55 -0400

    add task 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memcpy - copy n bytes from src to dest
+* @dest: destination of the copy
+* @src: source of the copy
+* @n: number of bytes to copy from src
+*
+*Return: pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned inr a;
+
+	for (a = 0 ; a < n ; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -43,6 +43,8 @@ char *leet(char *);
 
 char *_memset(char *s, char b, unsigned int n);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 
 
 #endif
