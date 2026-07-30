0ec27920cd590f9a28c84e62472e6e5aeb9034be
nycolemedina-lab <nycole.medina@gmain.com>
2026-07-30 12:04:46 -0400

    add task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_chessboard - print the chessboard
+* @a: pointer to an array of 8 characters
+*
+* Return: void
+*/
+void print_chessboard(char (*a)[8])
+{
+	int g, b;
+
+	for (g = 0; g < 8; g++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[g][b]);
+		}
+	_putchar('\0');
+	}
+	return (0);
+}
