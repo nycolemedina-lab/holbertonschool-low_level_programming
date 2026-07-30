018854d09d87653f0042f2c7a993d361822bc2d1
nycolemedina-lab <nycole.medina@gmain.com>
2026-07-30 11:28:27 -0400

    add task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* _strstr - seach a string for the number if bytes in the second string
+* @haystack: string to serch in
+* @needle: string to serch for
+*
+* Return: pointer to the beginning of the located substring, null if not found
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[d] != '\0'; b++)
+		{
+			if (haystack[a + b] != needle[b])
+			{
+				break;
+			}
+		}
+	}
+
+	if (needle[b] == '\0')
+	{
+		return (haystack + a);
+	}
+	return (0);
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -51,6 +51,8 @@ unsigned int _strspn(char *s, char *accept);
 
 char *_strpbrk(char *s, char *accept);
 
+char *_strstr(char *haystack, char *needle);
+
 
 
 #endif
