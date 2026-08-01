e31835fe8e5580b2f6686d36bde5995a294e4e90
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-01 18:51:07 -0400

    add task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+int check_prime(int n, int i);
+
+/**
+* is_prime_number - checks if an integer is a prime number
+* @n: number to evaluate
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (check_prime(n, 2));
+}
+
+/**
+* check_prime - helper finction to recursively check for divisors
+* @n: target number to evaluate
+* @i: current divisor to tast
+*
+* Return: 1 if prime, 0 if divisible by i
+*/
+int check_prime(int n, int i)
+{
+	if (i *i > n)
+	{
+		return (1);
+	}
+	if (n % 1 == 0)
+	{
+		return (0);
+	}
+	return (check_prime(n i + 1));
+}
