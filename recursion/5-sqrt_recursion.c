0f534c2931cad411ab8d4a1daf362b9840fd42df
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-01 18:08:12 -0400

    add task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int find_sqrt(int n, int i);
+
+/**
+* _sqrt_recursion - return the natural square root of a number
+* @n: number to find the square root of
+*
+* Retuen: natural square root, -1 if n does not have one
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, 1));
+}
+
+/**
+* find_sqrt - helper function to recursively find the square root
+* @n: target number
+* @i: current number root to test
+*
+* Return: i if square root found, -1 if no natural root exists
+*/
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return (find_sqrt(n, i + 1));
+
+}
