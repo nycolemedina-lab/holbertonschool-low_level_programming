207d0a9e03729b41296c48daf9cc30b3cadfe5f5
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-01 21:14:44 -0400

    add task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
 * @argc: the number of command line arguments
 * @argv: array containing the command line arguments
 *
-* Return - 0 if success
+* Return: 0 if success
 */
 int main(int argc, char *argv[])
 {
