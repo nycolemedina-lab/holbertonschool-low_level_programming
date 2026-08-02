81e90c6abf58dd5be7a863210f52d68e98614494
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-01 21:43:02 -0400

    add task 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - prints all arguments it receives
+* @argc: number of command line argument
+* @argv: array of command line arguments strings
+*
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; 1 < argc; i++)
+	{
+		printf(%c, argv[1]);
+	}
+	return (0);
+
