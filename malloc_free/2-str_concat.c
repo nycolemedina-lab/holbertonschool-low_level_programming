bbab734a1aa7d8246e7db565b29e43855836c805
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-07 11:53:58 -0400

    add task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,12 +17,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 
-	if (s2 == NUL)
+	if (s2 == NULL)
 		s2 ="";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-	for (lan2 = 0; s2[len2] != '\0'; len2++)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
 	concat = malloc((len1 + len2 + 1) * sizeof(char));
@@ -31,7 +31,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (a = 0; a < len1; a++)
-		cancat[a] = s1[a];
+		concat[a] = s1[a];
 
 	for (b = 0; b < len2; b++, a++)
 		cancat[a] = s2[b];
