c189edabde2863b4ef1cb5e018878632c2dd4a62
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-09 17:14:14 -0400

    add task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+* new_dog - creates a new struct
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of the dog
+*
+* Return: pointer to the new dog struc, of NULL on failure
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int len_name =0, len_owner = 0, a;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[len_name] != '\0')
+		len_name++;
+	while (owner[len_owner] != '\0')
+		len_owner++;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) * (len_name +1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (a = 0; a <= len_name; a++)
+		d->name[a] = name[a];
+
+	d->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (a = 0; a <= len_owner; a++)
+		d->owner[a] = owner[a];
+
+	d->age = age;
+
+	return (d);
+
+}
