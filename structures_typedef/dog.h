b34213a897627c381810031543f8b69d352c6fae
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-09 16:02:02 -0400

    add task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,12 @@
 * @age: second member (float)
 * @owner: third member (string)
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
