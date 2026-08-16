a3bcc201969a2214ea4d98de559a9a509133b159
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-16 01:14:58 -0400

    add task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* get_op_func - select the correct function to preform
+*		the arithmetic operation
+* @s: operetor string passed as argument
+*
+* Return: pointer to the function corresponding to the operator,
+*	or NULL if the operator is invalud or is NULL
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL && s != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+* op_add - calculate the sum of two integers
+* @a: first integer
+* @b: second integers
+*
+* Return: sum of a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - calculate the difference of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: difference of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - calculate the product of two integers
+* @a: first integer
+* @b: second integers
+*
+* Return: product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+* op_div - calculate the division of two integers
+* @a: first integer
+* @b: second integer
+*
+* Return: result of division of a by b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+* op_mod - calculate the reminder of division of teo integers
+* @a: first integers
+* @b: second integers
+*
+* Return: reminder of division of a by b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
