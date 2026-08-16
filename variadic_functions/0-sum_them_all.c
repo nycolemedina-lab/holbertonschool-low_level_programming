6a61c3246c67ae273facc03893523b7ada940081
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-16 16:44:44 -0400

    add task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* sum - calculates the sum of all its parameters
+* @n: numbers of parameters passed to the function
+*
+* Return: sum of all parameters, of 0 if n == 0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+
+	va_start(arg, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+	return (sum);
+}
