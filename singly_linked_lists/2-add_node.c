2e11249041d9fde3f983c9c0331dbfaeaf83069b
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-26 23:17:27 -0400

    add task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,11 +9,11 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	size_t *new_node;
+	list_t *new_node;
 	unsigned int len = 0;
 
 	if (head == NULL || str == NULL)
-		return (NULL)
+		return (NULL);
 
 	while (str[len] != '\0')
 		len++;
