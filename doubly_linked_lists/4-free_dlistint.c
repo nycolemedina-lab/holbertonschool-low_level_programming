bd2a64da4f036ce9c7c225734b5c98c815e4951a
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-29 01:05:32 -0400

    add task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* free_dlistint - free a dlistint_t list
+* @head: pointer to the head of the doubly linked list
+*
+* Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if(head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+
+}
