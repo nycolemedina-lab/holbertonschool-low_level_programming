c6825f5e762769b6d97f4c5504ff572bc0c57e6d
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-29 00:38:10 -0400

    add task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node = NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
