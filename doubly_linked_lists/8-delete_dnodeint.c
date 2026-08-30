d19ddbc0c0cab3391dca42afe01985544b838f9d
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 17:56:31 -0400

    add task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp;
-	unsigned int count;
+	unsigned int count = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
