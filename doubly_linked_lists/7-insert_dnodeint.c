f80b88f39f3e4428501e0bef46a01b10dd02ebf2
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 17:06:04 -0400

    add task 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+* insert_dnodeint_at_index - insert a new node at a given position
+* @h: doucle pointer to the head of the doucly linked list
+* @idx: index where the new node should be added (starting at 0)
+* @n: integer value for the new node
+*
+* Return: address of new node, NULL if it failed or if idx is out of bounds
+*/
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node, *temp;
+	unsigned int count = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	temp = *h
+
+	while (temp != NULL && count < idx - 1)
+	{
+		temp = temp->next;
+		count++;
+	}
+
+	if (temp == NULL)
+		return (NULL);
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof)(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+
+	temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
+
+
+}
+
