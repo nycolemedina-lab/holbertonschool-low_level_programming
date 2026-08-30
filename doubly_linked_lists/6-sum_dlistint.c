4809434668cbf0d53c0ad353d9d0a78f44b90610
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 16:16:31 -0400

    add task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* sum_dlistint - return the sum of all the data (n) if a dlistint_t list
+* @head: pointer to the head of the doubly linked list
+*
+* Return: sum of all data, or 0 is list empty
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int x = 0;
+
+	while (head != NULL)
+	{
+		x += head->n;
+		head = head->next;
+	}
+	return(x);
+}
