e81491b2c92a26a482fdc940a1f7eb19b7c32363
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 17:09:26 -0400

    add task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	temp = *h
+	temp = *h;
 
 	while (temp != NULL && count < idx - 1)
 	{
