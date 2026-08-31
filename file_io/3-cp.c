8222fed9a480cf1196255340826a9bf84524409b
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 23:09:08 -0400

    add task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,26 +11,37 @@ int main(int argc, char *argv[])
 	ssize_t bytes_rd, bytes_wr;
 	char buffer[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
-	f_from = open(av[1], O_RDONLY);
-	if (f_from == -1)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	f_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (f_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-	while ((r = read(f_from, buf, 1024)) > 0)
 	{
-		w = write(f_to, buf, r);
-		if (w == -1 || w != r)
-			dprintf(2, "Erro: Can't write to %s\n", av[2]), exit(99);
+		close(fd_from);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	if (r == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-
+	while ((bytes_rd = read(fd_from, buffer, 1024)) > 0)
+	{
+		bytes_wr = write(fd_to, buffer, bytes_rd);
+		if (bytes_wr == -1 || bytes_wr != bytes_rd)
+		{
+			dprintf(2, "Erro: Can't write to %s\n", av[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+	if (bytes_rd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_from);
+		exit(98);
+	}
 	if (close(f_from) == -1)
 		dprintf(2, "Error: Can't close fd %d\n", f_from), exit(100);
 	if (close(f_to) == -1)
