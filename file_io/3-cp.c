030747cccf7e517b74292392d0a9d9519565d272
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 23:12:27 -0400

    add task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (f_to == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
 		close(fd_from);
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -42,9 +42,9 @@ int main(int argc, char *argv[])
 		close(fd_from);
 		exit(98);
 	}
-	if (close(f_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_from), exit(100);
-	if (close(f_to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", f_to), exit(100);
+	if (close(fd_from) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+	if (close(fd_to) == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
