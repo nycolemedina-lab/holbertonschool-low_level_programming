37bf838b9385bbd6e04177c56971639e1ed3c398
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 22:27:52 -0400

    add task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+/**
+* main - copies the content of a file to another file
+* @argc: the number of arguments passed to the program
+* @argv: an array fo pointers to the arguments
+*
+* Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t bytes_rd, bytes_wr;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
+		exit(99);
+	}
+
+	while ((bytes_rd = read(fd_from, buffer, 1024)) > 0)
+	{
+		bytes_wr = write(fd_to, buffer, bytes_rd);
+		if (bytes_wr == -1 || bytes_wr != bytes_rd)
+                {
+			dprintf(STDERR_FILENO, "Erro: Can't write to %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
+
+/**
+* Close_fd - closes a file descriptor and handles failure
+* @fd: the file descriptor to close
+*/
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
