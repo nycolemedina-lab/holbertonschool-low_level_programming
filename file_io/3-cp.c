69665297d2043684d8d17af795e2b39eb0e09286
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-30 23:26:33 -0400

    add task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,24 +12,24 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	while ((bytes_rd = read(fd_from, buffer, 1024)) > 0)
 	{
 		bytes_wr = write(fd_to, buffer, bytes_rd);
 		if (bytes_wr == -1 || bytes_wr != bytes_rd)
 		{
-			dprintf(2, "Erro: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Erro: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
@@ -37,14 +37,14 @@ int main(int argc, char *argv[])
 	}
 	if (bytes_rd == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_from);
 		exit(98);
 	}
 	if (close(fd_from) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	if (close(fd_to) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
