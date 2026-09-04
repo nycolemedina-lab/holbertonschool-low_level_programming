ebc4cff452fa1975760d7bc074ec4e18b5325dbf
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-04 17:50:57 -0400

    add task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,10 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy, *key_copy;
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL || *key= == '\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdupvalue);
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
