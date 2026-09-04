7b8ea7c1946adf16c4fc3f0c511d24c7ac4ce877
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-04 18:11:12 -0400

    add task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -4,7 +4,8 @@
 * @ht: a pointer to the hash table
 * @key: the key you are looking for
 *
-* Return: the value associated with the element, or NULL if key couldn't be found
+* Return: the value associated with the element,
+* or NULL if key couldn't be found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
