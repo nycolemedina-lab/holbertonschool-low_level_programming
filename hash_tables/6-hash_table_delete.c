960cdaecd96ce9f5aa99b00ee56ed375f3ea4111
nycolemedina-lab <nycole.medina@gmain.com>
2026-09-05 12:49:43 -0400

    add task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+* hash_node_delete - delete a hash table and free all allocated memory
+* @ht: a pointer to the hash table to delete
+*
+* Return: void
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
